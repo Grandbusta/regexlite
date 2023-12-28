e335f6dbc2375004460ca87ef36907c247d7953d
Amatsagu <krzysztof.rapior@protonmail.com>
2023-12-28 18:06:22 +0100

    feat: export more errors

--- error.go
+++ error.go
@@ -5,4 +5,6 @@ import "errors"
 var (
 	ErrTooBig   = errors.New("regexlite: provided value is too big")
 	ErrTooSmall = errors.New("regexlite: provided value is too small")
+	ErrNotEmail = errors.New("regexlite: provided value is not a valid address")
+	ErrNotURL   = errors.New("regexlite: provided value is not a valid url")
 )
