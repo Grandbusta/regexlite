ca39a8c634cd5354ae0d656a3b44a98447193693
Amatsagu <krzysztof.rapior@protonmail.com>
2023-12-28 18:39:57 +0100

    feat: export more errors

--- error.go
+++ error.go
@@ -3,8 +3,11 @@ package regexlite
 import "errors"
 
 var (
-	ErrTooBig   = errors.New("regexlite: provided value is too big")
-	ErrTooSmall = errors.New("regexlite: provided value is too small")
-	ErrNotEmail = errors.New("regexlite: provided value is not a valid address")
-	ErrNotURL   = errors.New("regexlite: provided value is not a valid url")
+	ErrTooBig      = errors.New("regexlite: provided value is too big")
+	ErrTooSmall    = errors.New("regexlite: provided value is too small")
+	ErrNotEmail    = errors.New("regexlite: provided value is not a valid address")
+	ErrNotURL      = errors.New("regexlite: provided value is not a valid url")
+	ErrNoNumber    = errors.New("regexlite: provided value doesn't contain any number")
+	ErrNoUppercase = errors.New("regexlite: provided value doesn't contain any uppercase character")
+	ErrNoLowercase = errors.New("regexlite: provided value doesn't contain any lowercase character")
 )
