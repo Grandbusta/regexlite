c5e6eb055baf8319e0189f10f58f7ebebe3fcd1b
Amatsagu <krzysztof.rapior@protonmail.com>
2023-12-28 17:24:00 +0100

    feat: create wrapper structs with generics

---
+++ struct.go
@@ -0,0 +1,15 @@
+package regexlite
+
+type number interface {
+	~float32 | ~float64 | ~int | ~int8 | ~int16 | ~int32 | ~int64 | ~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 | ~complex64 | ~complex128
+}
+
+type nWrapper[T number] struct {
+	value T
+	err   error
+}
+
+type sWrapper struct {
+	value string
+	err   error
+}
